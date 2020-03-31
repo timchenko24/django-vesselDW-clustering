8e61f0cf76252b202066f3efdad2d737baad4ee6
Tim <timadraft@gmail.com>
2020-03-31 19:43:13 +0300

    urls hierarchy created

--- source/source/urls.py
+++ source/source/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from dw.views import index
 
 urlpatterns = [
+    path('dw/', index),
     path('admin/', admin.site.urls),
 ]
