5ae0a7c77d2d099bdfa8801c38b3e2fcbf73dc70
Tim <timadraft@gmail.com>
2020-03-31 19:57:14 +0300

    Port table created

--- source/dw/models.py
+++ source/dw/models.py
@@ -33,3 +33,12 @@ class Vessel(models.Model):
 class PortCountry(models.Model):
     id = models.UUIDField(primary_key=True, null=False, unique=True)
     name = models.CharField(max_length=50, null=False)
+
+
+class Port(models.Model):
+    id = models.UUIDField(primary_key=True, null=False, unique=True)
+    country = models.ForeignKey(PortCountry, on_delete=models.PROTECT, null=False)
+    name = models.CharField(max_length=50, null=False)
+    type = models.CharField(max_length=15, null=False)
+    longitude = models.DecimalField(max_digits=8, decimal_places=5, null=False)
+    latitude = models.DecimalField(max_digits=8, decimal_places=5, null=False)
