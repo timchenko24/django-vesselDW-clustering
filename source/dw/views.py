543898919eb47c44b99f5e503eebf15a84c3f6ba
Tim <timadraft@gmail.com>
2020-03-31 20:56:56 +0300

    vessel table page output

--- source/dw/views.py
+++ source/dw/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def index(request):
-    return HttpResponse('TEST')
+    vessels = Vessel.objects.all()
+    return render(request, 'dw/index.html', {'vessels': vessels})
