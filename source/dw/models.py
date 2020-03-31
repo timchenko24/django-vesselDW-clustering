57b94233caa630c78f695103010c3bf3f2452482
Tim <timadraft@gmail.com>
2020-03-31 19:53:20 +0300

    VesselType table created

--- source/dw/models.py
+++ source/dw/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class VesselType(models.Model):
+    id = models.UUIDField(primary_key=True, null=False, unique=True)
+    type = models.CharField(max_length=50, null=False)
