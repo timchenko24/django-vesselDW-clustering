6ffafada7ff6b75c101334cf1cb073bb9b08016f
Tim <timadraft@gmail.com>
2020-03-31 19:56:18 +0300

    Vessel table created

--- source/dw/models.py
+++ source/dw/models.py
@@ -14,3 +14,17 @@ class VesselBuild(models.Model):
 class VesselFlag(models.Model):
     id = models.UUIDField(primary_key=True, null=False, unique=True)
     flag = models.CharField(max_length=50, null=False)
+
+
+class Vessel(models.Model):
+    mmsi = models.IntegerField(primary_key=True, null=False, unique=True)
+    type = models.ForeignKey(VesselType, on_delete=models.PROTECT, null=False)
+    flag = models.ForeignKey(VesselFlag, on_delete=models.PROTECT, null=False)
+    build = models.ForeignKey(VesselBuild, on_delete=models.PROTECT, null=False)
+    name = models.CharField(max_length=70, null=False)
+    imo = models.IntegerField()
+    call_sign = models.CharField(max_length=10, null=False)
+    length = models.IntegerField()
+    width = models.IntegerField()
+    grt = models.IntegerField()
+    dwt = models.IntegerField()
