a9f61ac805fca5d35c3a225944c0c17900b64933
Tim <timadraft@gmail.com>
2020-04-08 19:54:50 +0300

    added controller for by_vesselType

--- source/dw/urls.py
+++ source/dw/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, by_vesselType
 
 
 urlpatterns = [
+    path('<int:type_id>', by_vesselType),
     path('', index)
 ]
--- source/dw/views.py
+++ source/dw/views.py
@@ -6,3 +6,11 @@ from .models import *
 def index(request):
     vessels = Vessel.objects.all()
     return render(request, 'dw/index.html', {'vessels': vessels})
+
+
+def by_vesselType(request, type_id):
+    vessels = Vessel.objects.filter(type=type_id)
+    types = VesselType.objects.all()
+    current_type = VesselType.objects.get(pk=type_id)
+    context = {'vessels': vessels, 'types': types, 'current_type': current_type}
+    return render(request, 'dw/by_vesselType.html', context)
