02585e484a4ba78242b243d4444da562a6591afc
Tim <timadraft@gmail.com>
2020-04-12 16:00:38 +0300

    vessels dir added

--- source/dw/urls.py
+++ source/dw/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, by_vesselType
+from .views import *
 
 
 urlpatterns = [
-    path('type/<uuid:type_id>/', by_vesselType, name='by_vesselType'),
+    path('vessels/type=<uuid:type_id>/', by_type, name='by_type'),
+    path('vessels/', vessels_index, name='vessels_index'),
     path('', index, name='index'),
 ]
