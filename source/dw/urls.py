528c1c134e4c6673f3c1f9cbb16b261144784356
Tim <timadraft@gmail.com>
2020-04-18 23:09:54 +0300

    404 page controller added

--- source/dw/urls.py
+++ source/dw/urls.py
@@ -5,5 +5,6 @@ from .views import *
 urlpatterns = [
     path('vessels/type=<uuid:type_id>/', by_type, name='by_type'),
     path('vessels/', vessels_index, name='vessels_index'),
+    path('<str:page>', other_page, name='other'),
     path('', index, name='index'),
 ]
--- source/dw/views.py
+++ source/dw/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import TemplateDoesNotExist
+from django.template.loader import get_template
 from .models import *
 
 # Create your views here.
 def index(request):
     return render(request, 'dw/index.html')
+
+
+def other_page(request, page):
+    try:
+        template = get_template('dw/{}.html'.format(page))
+    except TemplateDoesNotExist:
+        raise Http404
+    return HttpResponse(template.render(request=request))
+
 
 
 def vessels_index(request):
