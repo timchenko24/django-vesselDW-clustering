20b3a286c6815ed090096a440101e5ee212dbf6f
Tim <timadraft@gmail.com>
2020-04-19 19:49:08 +0300

    login url added

--- source/dw/urls.py
+++ source/dw/urls.py
@@ -5,6 +5,7 @@ from .views import *
 urlpatterns = [
     path('vessels/type=<uuid:type_id>/', by_type, name='by_type'),
     path('vessels/', vessels_index, name='vessels_index'),
+    path('accounts/login/', MainLoginView.as_view(), name='login'),
     path('<str:page>', other_page, name='other'),
     path('', index, name='index'),
 ]
